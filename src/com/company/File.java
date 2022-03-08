cc4664c1595f9ae5aa6f37e6d192430efe3eaced
Aleksey_Troskin <ltroskin20@gmail.com>
2022-03-08 17:23:46 +0300

    version3

--- src/com/company/File.java
+++ src/com/company/File.java
@@ -1,46 +1,57 @@
 package com.company;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class File {
 
+    public static HashMap<Object, String> dictionary = new HashMap<>();
+    public static Scanner in = new Scanner(System.in);
+    private static final String PATH = "src/resources/File1.txt";
+
+    public static void convertHashMap() throws IOException {
+        Scanner filescan = new Scanner(new FileReader(PATH));
+        while (filescan.hasNextLine()) {
+            String[] columns = filescan.nextLine().split(":");
+            dictionary.put(columns[0], columns[1]);
+        }
+    }
+
     public File() {
     }
 
-    private static final String PATH = "resources/File1.txt";
 
     public static void FileRead() throws IOException{
         try (BufferedReader in = new BufferedReader(new FileReader(PATH))) {
             String line;
             while ((line = in.readLine()) != null) {
-                System.out.println(line);
+                for (HashMap.Entry entry : dictionary.entrySet()) {
+                    System.out.println(entry.getKey() + ":" + entry.getValue());
+                }
             }
         }
     }
 
     public static void AddFile(){
 
-        Scanner in = new Scanner(System.in);
-        String text = in.nextLine();
 
-
-        try {
-            FileWriter writer = new FileWriter(PATH, true);
+        System.out.println("Введите ключ: ");
+        String key = in.nextLine();
+        System.out.println("Введите значение: ");
+        String value = in.nextLine();
+        dictionary.put(key,value);
+            /*FileWriter writer = new FileWriter(PATH, true);
             BufferedWriter bufferWriter = new BufferedWriter(writer);
             bufferWriter.write(text+"\r\n");
-            bufferWriter.close();
-        }
-        catch (IOException e) {
-            System.out.println();
-        }
+            bufferWriter.close();*/
     }
 
     public static void Search() throws IOException {
-        Scanner in = new Scanner(System.in);
-        String searchWord = in.nextLine();
-        FileInputStream fis = new FileInputStream(PATH);
+        String key = in.nextLine();
+        String searchWord = dictionary.get(key);
+        System.out.println(searchWord);
+        /*FileInputStream fis = new FileInputStream(PATH);
         byte[] content = new byte[fis.available()];
         fis.read(content);
         fis.close();
@@ -61,12 +72,12 @@ public class File {
         }
         if (k<1){
             System.out.println("слово " +searchWord +" не найдено");
-        }
+        }*/
     }
 
 
     public static void Delete() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(PATH)); PrintWriter writer = new PrintWriter(new FileWriter(PATH))) {
+        /*try (BufferedReader reader = new BufferedReader(new FileReader(PATH)); PrintWriter writer = new PrintWriter(new FileWriter(PATH))) {
 
             //File file = new File();
             int current = 0;
@@ -80,6 +91,12 @@ public class File {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }*/
+        System.out.print("Введите ключ значения,которое хотите удалить: ");
+        String keyDel = in.nextLine();
+        dictionary.remove(keyDel);
+        for (HashMap.Entry entry : dictionary.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
         }
     }
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -5,13 +5,48 @@ import java.util.Objects;
 import java.util.Scanner;
 
 
-
 public class Menu{
 
 
+    public static Scanner in = new Scanner(System.in);
+
+
+    public static void select() throws IOException{
+        System.out.println("Выберите словарь");
+        String select = "";
+        String lim = "3";
+        int dictionary = 0;
+        while (!Objects.equals(lim, select)) {
+            System.out.println("1.Словарь со словами длиной 4 символа и эти символы только только буквы латинской раскладки");
+            System.out.println("2.Словарь со словами длиной 5 символов и эти символы только цифры");
+            System.out.println("3.Выход из приложения");
+            select = in.next();
+            try {
+                dictionary = Integer.parseInt(select);
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный ввод");
+            }
+
+            switch (dictionary) {
+                case 1:
+                    action();
+                    break;
+                case 2:
+                    File.Delete();
+                    break;
+                case 3:
+                    System.exit(0);
+            }
+        }
+
+
+    }
+
 
 public static void action() throws IOException {
-    Scanner scan = new Scanner(System.in);
+
+    File.convertHashMap();
+
     int method = 0;
     String choice ="";
     String limit = "5";
@@ -22,8 +57,8 @@ public static void action() throws IOException {
         System.out.println("2. Для удаления записи в словаре введите 2");
         System.out.println("3. Для поиска записи по ключу в словаре введите 3");
         System.out.println("4. Для добавления записи в словарь введите 4");
-        System.out.println("5. Для выхода из приложения введите 5");
-        choice = scan.next();
+        System.out.println("5. Для возврата к выбору словаря введите 5");
+        choice = in.next();
 
         try {
             method = Integer.parseInt(choice);
@@ -48,12 +83,10 @@ public static void action() throws IOException {
                 break;
             case 4:
                 // вызов метода 4
-                System.out.print("Введите данные: ");
-
                 File.AddFile();
                 break;
             case 5:
-                System.exit(0);
+                select();
         }
     }
         System.out.println("До свидания!");
